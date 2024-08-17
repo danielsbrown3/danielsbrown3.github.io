8babc4fa529937f8d8ab72429343608d2fde120c
danielsbrown3 <dsbrown@ucsd.edu>
2024-08-17 11:18:36 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Website loaded successfully!');
 });
+
+ document.querySelector('.down-arrow').addEventListener('click', function() {
+      document.getElementById('target-section').scrollIntoView({ behavior: 'smooth' });
+    });
